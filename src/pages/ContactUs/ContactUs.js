53e6eb8eb0c962c5900c5fb0ffc4ccfdc9a22269
unknown <mithunbaruaexp@gmail.com>
2021-10-19 23:26:37 +0600

    update

---
+++ src/pages/ContactUs/ContactUs.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ContactUs.css'
+
+
+const ContactUs = () => {
+    return (
+        <div className="main-container">
+            <div className='sub-container'>
+                <span>
+                    <p className='text-left'>Intersested?</p>
+                    <h1>CONTACT US AND SCHEDULE YOUR VISIT</h1>
+                </span>
+                <button className='btn-contact'>
+                    Cotact us
+                </button>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default ContactUs;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import ContactUs from '../ContactUs/ContactUs';
 import Services from '../Services/Services';
 import Team from '../Team/Team';
 
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Team></Team>
             <Services></Services>
+            <ContactUs></ContactUs>
             
         </div>
     );
